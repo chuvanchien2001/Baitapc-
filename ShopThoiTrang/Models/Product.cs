98ba35f7cce8caa9e482118720d51eb96bd17760
chuvanchien2001 <chuvanchien2001barca@gmail.com>
2022-12-25 16:34:26 +0700

    first commit

---
+++ ShopThoiTrang/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopThoiTrang.Models
+{ 
+    [Table ("Product")]
+    public class Product
+    {
+
+    }
+}
